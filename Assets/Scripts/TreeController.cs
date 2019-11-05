51e065b8a5abb47174e960238cd85e4d082b344e
Helpsypoo <justin.r.helps@gmail.com>
2019-11-04 18:11:53 -0600

    Have trees manage fruits to smooth forest regrowth

--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -5,17 +5,80 @@ using UnityEngine;
 public class TreeController : HarvestableController
 {
     private float logSpawnHeight = 1.0f;
+    private const int fruitCapacity = 4;  //Trees have room for four fruits
+    public GameObject[] fruits = new GameObject[fruitCapacity];
+
+    public GameObject mango;
+    public static int mangoYield = 1;
+    public float mangoDistance = 1.3f;
+    private float mangoHeight = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+      //Debug.Log(fruits[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void GrowMangoes()
+    {
+      int numMangoes = GetNumMangoes();
+      for (int i = 0; i < mangoYield - numMangoes; i++)
+      {
+        AddMango();
+      }
+    }
+
+    void AddMango()
+    {
+      GameObject newMango = Instantiate(mango, gameObject.transform);
+      //mangoes[mangoIndex] = newMango;
+      //TODO rotate mango
+      //TODO put rotation and translation in Instantiate call
+
+      int positionIndex = GetOpenFruitIndex();
+      fruits[positionIndex] = newMango;
+
+      Vector3 mangoPos = new Vector3(mangoDistance, mangoHeight, 0);
+      for (int i = 0; i < positionIndex; i++)
+      {
+        mangoPos = Quaternion.Euler(0, 90, 0) * mangoPos;
+      }
+      newMango.transform.Translate(mangoPos);
+    }
+
+    int GetOpenFruitIndex()
+    {
+      //Almost certainly a more compact way to do this
+      List<int> openSpaces = new List<int>();
+      for (int i = 0; i < fruitCapacity; i++)
+      {
+        if (fruits[i] == null)
+        {
+          openSpaces.Add(i);
+        }
+      }
+      int openSpaceIndex = Random.Range(0, openSpaces.Count);
+      return openSpaces[openSpaceIndex];
+    }
+
+    int GetNumMangoes()
+    {
+      //Almost certainly a more better way to do this
+      int numMangoes = 0;
+      for (int i = 0; i < fruitCapacity; i++)
+      {
+        if (fruits[i] != null)
+        {
+          numMangoes++;
+        }
+      }
+      return numMangoes;
     }
 
     public override GameObject HandleHarvest()
@@ -28,10 +91,17 @@ public class TreeController : HarvestableController
       GameObject log = Instantiate(logPrefab, logSpawnPos, logPrefab.transform.rotation);
 
       //TODO make this work for more than or less than one fruit
-      GameObject fruit = gameObject.transform.GetChild(0).gameObject;
-      fruit.transform.parent = null;
-      Rigidbody fruitRb = fruit.GetComponent<Rigidbody>();//gameObject.GetComponentInChildren<Rigidbody>();
-      fruitRb.useGravity = true;
+      for (int i = 0; i < fruits.Length; i++)
+      {
+        if (fruits[i] != null)
+        {
+          fruits[i].transform.parent = null;
+          Rigidbody fruitRb = fruits[i].GetComponent<Rigidbody>();//gameObject.GetComponentInChildren<Rigidbody>();
+          fruitRb.useGravity = true;
+          fruits[i] = null;
+        }
+      }
+
 
       harvested = true;
       gameObject.SetActive(false);
