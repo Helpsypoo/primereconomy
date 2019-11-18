56979acf61d9fc9167d54ccc7b57996cbb97aef2
Helpsypoo <justin.r.helps@gmail.com>
2019-11-18 08:57:43 -0600

    Add recorder component and make everything a child of EconomyManager. Shelving for now because spawning from recording isn't trivial with my current knowledge, and it's not really needed for this sim.

---
+++ Assets/Scripts/RecordTransformHierarchy.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class RecordTransformHierarchy : MonoBehaviour
+{
+    public AnimationClip clip;
+
+    private GameObjectRecorder m_Recorder;
+
+    void Start()
+    {
+        // Create recorder and record the script GameObject.
+        m_Recorder = new GameObjectRecorder(gameObject);
+
+        // Bind all the Transforms on the GameObject and all its children.
+        m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
+        //m_Recorder.BindComponentsOfType<Animator>(gameObject, true);
+    }
+
+    void LateUpdate()
+    {
+        if (clip == null)
+            return;
+
+        // Take a snapshot and record all the bindings values for this frame.
+        m_Recorder.TakeSnapshot(Time.deltaTime);
+    }
+
+    void OnDisable()
+    {
+        if (clip == null)
+            return;
+
+        if (m_Recorder.isRecording)
+        {
+            // Save the recorded session to the clip.
+            m_Recorder.SaveToClip(clip);
+        }
+    }
+}
